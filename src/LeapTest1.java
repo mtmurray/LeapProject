950e6c44b09a15ac2aa763fba8921155bf1ba6ff
Matthew Murray <2088324M@student.gla.ac.uk>
2018-07-14 22:16:34 +0100

    colour feedback & distance check

--- src/LeapTest1.java
+++ src/LeapTest1.java
@@ -3,6 +3,8 @@ import java.io.OutputStream;
 
 import com.fazecast.jSerialComm.*;
 import java.util.Random;
+import java.util.Scanner;
+
 import com.leapmotion.leap.*;
 import com.leapmotion.leap.Gesture.State;
 
@@ -10,11 +12,16 @@ class LeapListener extends Listener
 {
 	private static SerialPort p = SerialPort.getCommPort("COM3");
 	private Vector targetPoint;
-	private double alpha = 0.33;
+	private double alpha = 0.40;
 	private double filtered_proximity = 0.0;
+	private String trial_type;
+	private double previous_proximity = 0.0;
 	
 	public void onInit(Controller controller) {
-		//System.out.println("Initialised");
+		//Establish trial type: brightness ("b"), colour ("c"), or position ("p")
+		System.out.println("Please specify feedback type for this trial (b, c, p):");
+		Scanner in = new Scanner(System.in);
+		trial_type = in.nextLine();
 		
 		//Set up connection to Arduino
 		p.setBaudRate(115200);
@@ -58,14 +65,24 @@ class LeapListener extends Listener
 	}
 	
 	public void onExit(Controller controller) {
-		//System.out.println("Exited");
+		
+		OutputStream output = p.getOutputStream();
+		
+		try 
+		{
+			output.write("3".getBytes());
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	public void onFrame(Controller controller) {
 		//frames are processed here - data for each frame
 		Frame frame = controller.frame(); //frame from the controller
 		
-		//Interaction box measurements - not needed in final program
+		//Interaction box measurements
 		//InteractionBox ib = frame.interactionBox();
 		//System.out.println("Box height: " + ib.height());
 		//System.out.println("Box width: " + ib.width());
@@ -73,8 +90,7 @@ class LeapListener extends Listener
 		
 		//Isolate end of middle finger, store position as vector
 		FingerList fingers = frame.fingers();
-		Finger middle = fingers.get(2);
-		Bone bone = middle.bone(Bone.Type.TYPE_DISTAL);
+		Bone bone = fingers.get(2).bone(Bone.Type.TYPE_DISTAL);
 		Vector tracking = bone.nextJoint();
 		
 		//proximity of finger tip to target
@@ -91,23 +107,45 @@ class LeapListener extends Listener
 			closenessPercentage = 0; //score is 0 if user's finger tip is outside sphere's radius
 		}
 		
-		//Information for testing purposes: positions of target and finger, proximity, and sphere-based score
+		//Information for testing purposes
 		//System.out.println("TARGET: " + targetPoint.getX() + "," + targetPoint.getY() + "," + targetPoint.getZ());
 		//System.out.printf("TRACKING: %.2f, %.2f, %.2f\n", tracking.getX(), tracking.getY(), tracking.getZ());
 		//System.out.println("Proximity = " + proximity);
 		//System.out.println("Filtered Proximity = " + filtered_proximity);
-		System.out.println("Closeness score = " + closenessPercentage);
+		//System.out.println("Closeness score = " + closenessPercentage);
 		//System.out.println("-------------------------------------");
 		//System.out.printf("%05.1f,%05.1f\n", proximity, filtered_proximity);
 		
+		if (trial_type.equals("b") && ((filtered_proximity >= previous_proximity + 10 || filtered_proximity <= previous_proximity - 10)))
+		{
+			updateBrightness(closenessPercentage);
+		}
+		else if (trial_type.equals("c") && ((filtered_proximity >= previous_proximity + 10 || filtered_proximity <= previous_proximity - 10))) 
+		{
+			updateColour(closenessPercentage);
+		}
+		else if (trial_type.equals("p")) 
+		{
+			//yet to implement position feedback
+		}
+		else 
+		{
+			return;
+		}
 		
+		//set current trial's proximity to act as previous proximity during next trial
+		previous_proximity = filtered_proximity;
+	}
+	
+	public void updateBrightness(float pct)
+	{
 		//Output brightness value to Arduino, reflecting closeness percentage
 		String brightCmd = "2";
 		OutputStream output = p.getOutputStream();
 		
 		int lightsOff = 0; //when lights off, rgb values all = 0
 		int fullBrightness = 255; //at full brightness, all rgb values = 255
-		int brightness = (int) simple_interpolate(lightsOff, fullBrightness, closenessPercentage);
+		int brightness = (int) simple_interpolate(lightsOff, fullBrightness, pct);
 		brightCmd += " " + Integer.toString(brightness) + " " + Integer.toString(brightness) + " " + Integer.toString(brightness) + "\n";
 		
 		try 
@@ -118,8 +156,37 @@ class LeapListener extends Listener
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	public void updateColour(float pct) 
+	{
+		String colourCmd = "2";
+		OutputStream output = p.getOutputStream();
 		
+		int blue = 0; //level of blue does not need to change
 		
+		//red rg values
+		int r1 = 255;
+		int g1 = 0;
+		
+		//green rg values
+		int r2 = 0;
+		int g2 = 255;
+		
+		//values to update LEDs
+		int newR = (int) simple_interpolate(r1, r2, pct);
+		int newG = (int) simple_interpolate(g1, g2, pct);
+		
+		colourCmd += " " + Integer.toString(newR) + " " + Integer.toString(newG) + " " + Integer.toString(blue);
+		
+		try 
+		{
+			output.write(colourCmd.getBytes());
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	public float simple_interpolate(int a, int b, float pct) {
